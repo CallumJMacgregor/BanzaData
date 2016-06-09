a598b3c1a3922f80912fd668734c7a4a69c7c673
CallumJMacgregor <callumjmacgregor@gmail.com>
2016-06-09 12:02:40 +0100

    Load libraries

--- PreparingData.R
+++ PreparingData.R
@@ -1,5 +1,19 @@
 #########################################
-####   Preparing data for analysis   ####
+####   Script for preparing data for analysis   ####
 #########################################
 
+
+### Clear the workspace
+rm(list=ls())
+
+
+### load the libraries you need
+j <- c("reshape2","ggplot2")
+
+new.packages <- j[!(j %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+
+library(reshape2)
+library(ggplot2)
 
