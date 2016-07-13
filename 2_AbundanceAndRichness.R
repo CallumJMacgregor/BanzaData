86f57fd8dc43efa4b9a7424efbf66eeb34fd0ade
CallumJMacgregor <callumjmacgregor@gmail.com>
2016-07-13 15:53:41 +0100

    Analysis of plant abundance & richness

--- 2_AbundanceAndRichness.R
+++ 2_AbundanceAndRichness.R
@@ -9,7 +9,7 @@ rm(list=ls())
 
 ### install if necessary and then load the libraries you need
 
-j <- c("lme4","car","ggplot2","RVAideMemoire","arm","MASS","plyr")
+j <- c("lme4","car","ggplot2","RVAideMemoire","arm","MASS","plyr","reshape2","vegan")
 
 new.packages <- j[!(j %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
@@ -24,7 +24,8 @@ library(glmmADMB)
 
 
 ### load up Callum's custom set of functions
-source("CheckResidsFunction.R")
+f <- c("CheckResidsFunction.R","CheckConvergenceFunction.R","SpecAccumFunctions.R")
+lapply(f, source)
 
 
 ### Moths
@@ -69,15 +70,16 @@ plot(Count ~ Month, data = dframe3)
 # test effects of Treatment and Season on abundance
 # first plot distribution of abundance
 hist(dframe3$Count)
+summary(dframe3$Count)
 
-# data are Poisson and possibly overdispersed
+# data appear Poisson and possibly overdispersed
 # a quick check for overdispersion; in a regular Poisson distribution, mean=variance:
 mean(dframe3$Count)
 var(dframe3$Count)  # var > mean therefore data are overdispersed
 
 
 ### Data appear overdispersed, so we will try two types of model in addition to Poisson: quasi-Poisson, and negative binomial
-### However no zeroes in data, so zero-inflated models would be inappropriate
+### However not many zeroes in data, so zero-inflated models would be inappropriate
 
 # construct models using Date and Site as random effects
 # random effects are factors that might affect the output variable, but not in a way that is interesting to us
@@ -100,61 +102,66 @@ summary(model1P)
 drop1(model1P, test="Chisq")  
 
 
-
 # check the model's residuals
 # this custom function produces a selection of plots that you can scroll through to check that residuals look ok
+# you need to pass a model and you can optionally pass up to two explanatory variables as well
 # you want residuals that are roughly normally distributed around zero with no obvious trends
-chkres(model1P)  # these residuals are really not bad considering the potential overdispersion
-
-
-# QuasiPoisson model using MASS
-
-model1Q <- glmmPQL(Count ~ Treatment * Season,
-                   random = list(~1|Year, ~1|Site, ~1|Date),
-                   family = quasipoisson (link = "log"),
-                   data = dframe3)
-
-
-summary(model1Q)
-Anova(model1Q, type="III")  # drop1 doesn't work properly with this model class so we use a Type III Anova instead
-
-# check residuals
-# this function produces a subset of the previous plots that are available for this model class
-chkres.PQL(model1Q) # these look markedly worse
+# you want residuals that are roughly the same for each level of your treatments
+chkres(model1P,dframe3$Treatment,dframe3$Season)  # these residuals are really not bad considering the potential overdispersion
 
 
 
-# Negative binomial model using lme4
+# try a log-transformation
+hist(log(dframe3$Count+1,10)) # this looks quite normally distributed
 
-model1NB <- glmer.nb(Count ~ Treatment * Season # fixed effects
-                     + (1|Year) + (1|Site) + (1|Date), # random effects
-                     data = dframe3)
+dframe3$tCount <- log(dframe3$Count+1,10) # log-transform the data, adding 1 (as 0 cannot be log transformed)
 
-summary(model1NB)
-drop1(model1NB, test= "Chisq") # glmer.nb produces the same model class as glmer so we can treat it the same
 
-chkres(model1NB) # these are worse again
+plot(tCount ~ Treatment, data = dframe3)
+plot(tCount ~ Season, data = dframe3)
+plot(tCount ~ Month, data = dframe3)
 
-### choose from these candidate error families
-### none are perfect so we use the simplest model with best fit - Poisson
 
-summary(model1P)
-drop1(model1P, test="Chisq")
+
+# Gaussian
+
+model1G <- lmer(tCount ~ Treatment*Season
+                + (1|Year) + (1|Site) + (1|Date), # random effects
+                data = dframe3)
+
+summary(model1G)
+drop1(model1G, test="Chi")
+
+chkres(model1G,dframe3$Treatment,dframe3$Season) # residuals are pretty good 
+
+# but interaction term non-significant so reconstruct without this
+
+model1Ga <- lmer(tCount ~ Treatment + Season
+                 + (1|Year) + (1|Site) + (1|Date), # random effects
+                 data = dframe3)
+
+summary(model1Ga)
+drop1(model1Ga, test="Chi")
+
+chkres(model1Ga,dframe3$Treatment,dframe3$Season) # we have a winner 
+
+
 
 
 ### Species Richness ###
 
 # plot richness against Treatment and Season
-plot(SpeciesRichness ~ Treatment, data = dframe3)
-plot(SpeciesRichness ~ Season, data = dframe3)
+plot(SpeciesRichness ~ interaction(Treatment,Season), data = dframe3)
 
 
 # test effects of Treatment and Season on abundance
 # first plot distribution of abundance
 hist(dframe3$SpeciesRichness)
+hist(log(dframe3$SpeciesRichness+1, 10)) # There are zeroes in this data which can't be log transformed
+summary(dframe3$SpeciesRichness)
+
 
 # data are Poisson and possibly overdispersed
-# a quick check for overdispersion; in a regular Poisson distribution, mean=variance:
 mean(dframe3$SpeciesRichness)
 var(dframe3$SpeciesRichness)  # var > mean therefore data are overdispersed
 
@@ -166,19 +173,66 @@ model2P <- glmer(SpeciesRichness ~ Treatment * Season # fixed effects
                  family = poisson (link = "log"),
                  data = dframe3)
 
+# warning about model convergence so check it again, using:
+
+chkconv(model2P) # model is acceptable
+
 # inspect and test the model
 summary(model2P)
-
-# the best test of a GLMM is a Likelihood Ratio Test, which compares the fit of the model to if each term was removed one at a time using Chi-squared tests
-# to run an LRT, use the drop1 function (i.e. drop 1 term at a time), specifying Chi-squared
 drop1(model2P, test="Chisq")  
 
-
-
 # check the model's residuals
-# this custom function produces a selection of plots that you can scroll through to check that residuals look ok
-# you want residuals that are roughly normally distributed around zero with no obvious trends
-chkres(model2P)  # these residuals do appear to have a slight negative trend so they are not ideal
+chkres(model2P,dframe3$Treatment,dframe3$Season)  # these residuals aren't great for the Treatment variable
+
+# try a QP for comparison's sake
+
+# QuasiPoisson model using MASS
+
+model2Q <- glmmPQL(SpeciesRichness ~ Treatment * Season,
+                   random = list(~1|Year, ~1|Site, ~1|Date),
+                   family = quasipoisson (link = "log"),
+                   data = dframe3)
+
+
+summary(model2Q)
+Anova(model2Q, type="III")  # drop1 doesn't work properly with this model class so we use a Type III Anova instead
+
+# check residuals
+# this function produces a subset of the previous plots that are available for this model class
+chkres.PQL(model2Q,dframe3$Treatment,dframe3$Season) # these look a little better but are positive skewed
+
+
+# try a log-transformation
+hist(log(dframe3$SpeciesRichness+1,10)) # this looks quite normally distributed
+
+dframe3$tSpeciesRichness <- log(dframe3$SpeciesRichness+1,10) # log-transform the data, adding 1 (as 0 cannot be log transformed)
+
+plot(tSpeciesRichness ~ interaction(Treatment,Season), data = dframe3)
+
+
+# Gaussian
+
+model2G <- lmer(tSpeciesRichness ~ Treatment*Season
+                + (1|Year) + (1|Site) + (1|Date), # random effects
+                data = dframe3)
+
+summary(model2G)
+drop1(model2G, test="Chi")
+
+chkres(model2G,dframe3$Treatment,dframe3$Season) # residuals are fairly good 
+
+# but interaction term marginally non-significant so reconstruct without this
+
+model2Ga <- lmer(tSpeciesRichness ~ Treatment + Season
+                 + (1|Year) + (1|Site) + (1|Date), # random effects
+                 data = dframe3)#
+
+summary(model2Ga)
+drop1(model2Ga, test="Chi")
+
+chkres(model2Ga,dframe3$Treatment,dframe3$Season) # a very slight hint of positive trend, but better than the others 
+
+
 
 
 
@@ -201,7 +255,10 @@ summary(dframe4r)
 dframe4r$SpeciesRichness <- ifelse(dframe4r$PlantSpecies=="none",0,1)
 
 # summarise the dataframe to how many individuals of each species in each sample
-dframe5 <- ddply(dframe4r, .(Date,Site,Transect,Treatment,Sample,Season,Month,Year), numcolwise(sum))
+dframe5 <- ddply(dframe4r, .(Date,Site,Transect,Treatment,Sample,Season,Month,Year), numcolwise(mean))
+
+dframe5SR <- ddply(dframe4r, .(Date,Site,Transect,Treatment,Sample,Season,Month,Year), numcolwise(sum))
+
 
 dframe5$Month <- ordered(dframe5$Month, levels=c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"))
 
@@ -216,50 +273,152 @@ plot(PlantCoverage ~ Year, dframe5)
 plot(PlantCoverage ~ interaction(Treatment,Season), dframe5)
 
 hist(dframe5$PlantCoverage)
+hist(log(dframe5$PlantCoverage+1,10))
+summary(dframe5$PlantCoverage)
 
-# possibly overdispersion (but not sure...)
+# possibly overdispersion, possibly zero-inflation (1st quartile is 0)
+# because it's average data it's non-integer so Poisson won't work
+# but the log looks close to Gaussian so let's try that first
 mean(dframe5$PlantCoverage)
 var(dframe5$PlantCoverage)  # var > mean therefore data are overdispersed
 
+# Gaussian
 
-# Poisson model
-model5P <- glmer(PlantCoverage ~ Treatment*Season*Year
-                 + (1|Site) + (1|Date),
-                 family = poisson (link="log"),
+dframe5$tPlantCoverage <- log(dframe5$PlantCoverage + 1,10)
+summary(dframe5$tPlantCoverage)
+hist(dframe5$tPlantCoverage)
+
+
+model5G <- lmer(tPlantCoverage ~ Treatment*Season
+                 + (1|Year) + (1|Site) + (1|Date),
                  data = dframe5)
+
+summary(model5G)
+drop1(model5G, test = "Chi")
+
+chkres(model5G,dframe5$Treatment,dframe5$Season) # these look ok-ish 
+# but have a fairly clear negative trend and problems with the fit of season
+
+
+# Poisson
+# can't deal with non-integers so we'll have to use total rather than mean coverage
+
+model5P <- glmer(PlantCoverage ~ Treatment*Season
+                 + (1|Year) + (1|Site) + (1|Date),
+                 family = poisson (link = "log"),
+                 data = dframe5SR)
 
 summary(model5P)
 drop1(model5P, test = "Chi")
 
-chkres(model5P)
+chkres(model5P,dframe5SR$Treatment,dframe5SR$Season) # these are pretty bad
+
+# nbinom
+
+model5NB <- glmer.nb(PlantCoverage ~ Treatment*Season
+                  + (1|Year) + (1|Site) + (1|Date),
+                  data = dframe5SR)
+
+chkconv(model5NB)
+
+summary(model5NB)
+drop1(model5NB, test = "Chi")
+
+chkres(model5NB,dframe5SR$Treatment,dframe5SR$Season) # these are pretty bad as well
+
+# we need to bite the bullet and use zero-inflated models
+# zero-inflated poisson
+
+#model5ZIP <- glmmadmb(PlantCoverage ~  Treatment*Season
+#                      + (1|Year) + (1|Site) + (1|Date), #Random effects
+#                      zeroInflation=TRUE,
+#                      family = "poisson",
+#                      link = "log",
+#                      data = dframe5SR)
+
+#summary(model5ZIP)
+
+#Anova(model5ZIP, type="III")
+#drop1(model5ZIP, test="Chisq")
+
+#chkres.zi(model5ZIP,dframe5SR$Treatment,dframe5SR$Season) # these are pretty horrible
+
+
+# zero-inflated nbinom
+
+#model5ZINB <- glmmadmb(PlantCoverage ~  Treatment*Season
+#                       + (1|Year) + (1|Site) + (1|Date), #Random effects
+#                       zeroInflation=TRUE,
+#                       family = "nbinom",
+#                       link = "log",
+#                       data = dframe5SR)
+
+#summary(model5ZINB)
+
+#Anova(model5ZINB, type = "III")
+#drop1(model5ZINB, test="Chisq")
+
+#chkres.zi(model5ZINB,dframe5SR$Treatment,dframe5SR$Season)
+
+
+
+# actually everything is substantially worse than that log-Gaussian model at the start
+# let's just use that one.
+
+summary(model5G)
+drop1(model5G, test = "Chi")
+
+
 
 
 # Floral Species Richness
-plot(SpeciesRichness ~ Treatment, dframe5)
-plot(SpeciesRichness ~ Season, dframe5)
-plot(SpeciesRichness ~ Month, dframe5)
-plot(SpeciesRichness ~ interaction(Treatment,Season), dframe5)
+plot(SpeciesRichness ~ Treatment, dframe5SR)
+plot(SpeciesRichness ~ Season, dframe5SR)
+plot(SpeciesRichness ~ Month, dframe5SR)
+plot(SpeciesRichness ~ interaction(Treatment,Season), dframe5SR)
 
-hist(dframe5$SpeciesRichness)
+hist(dframe5SR$SpeciesRichness)
 
 # possibly overdispersion (but not sure...)
-mean(dframe5$SpeciesRichness)
-var(dframe5$SpeciesRichness)  # var > mean therefore data are overdispersed
+mean(dframe5SR$SpeciesRichness)
+var(dframe5SR$SpeciesRichness)  # var > mean therefore data are overdispersed
 
 
 # Poisson model
 model6P <- glmer(SpeciesRichness ~ Treatment*Season
                  + (1|Year) + (1|Site) + (1|Date),
                  family = poisson (link="log"),
-                 data = dframe5)
+                 data = dframe5SR)
 
 summary(model6P)
 drop1(model6P, test = "Chi")
 
-chkres(model6P)
+chkres(model6P,dframe5SR$Treatment,dframe5SR$Season) # these are BAAAD - they look like the data needs a log transformation
 
 
+# negative binomial
+model6NB <- glmer.nb(SpeciesRichness ~ Treatment*Season
+                     + (1|Year) + (1|Site) + (1|Date),
+                     data = dframe5SR)
 
+summary(model6NB)
+drop1(model6NB, test="Chi")
+
+chkres(model6NB,dframe5SR$Treatment,dframe5SR$Season)
+
+
+# try a log-transformed gaussian as that worked previously
+dframe5SR$tSpeciesRichness <- log(dframe5SR$SpeciesRichness+1,10)
+hist(dframe5SR$tSpeciesRichness)
+
+model6G <- lmer(tSpeciesRichness ~ Treatment*Season
+                + (1|Year) + (1|Site) + (1|Date),
+                data = dframe5SR)
+
+summary(model6G)
+drop1(model6G, test = "Chi")
+
+chkres(model6G,dframe5SR$Treatment,dframe5SR$Season) # these are not terrible, though the zeroes in summer continue to have a visible effect
 
 
 
