ec480127a2aa4cae672c414636ae9239d92748a9
CallumJMacgregor <callumjmacgregor@gmail.com>
2016-07-13 15:54:21 +0100

    Added plotting against up to 2 independent variables

--- CheckResidsFunction.R
+++ CheckResidsFunction.R
@@ -1,4 +1,4 @@
-chkres <- function(model) {
+chkres <- function(model,variable1,variable2) {
   require(RVAideMemoire)
   sresid <- resid(model, type = "deviance")
   hist(sresid)
@@ -7,23 +7,26 @@ chkres <- function(model) {
   plot(model)                                  # gives a heteroscedasticity plot #fans out, not good
   require(arm)
   binnedplot(fitted(model),resid(model))
+  plot(sresid ~ variable1)
+  plot(sresid ~ variable2)
 }
 
 
 
 
-chkres.PQL <- function(model) {
+chkres.PQL <- function(model,variable1,variable2) {
   require(RVAideMemoire)
   plot(model)
   presid <- resid(model, type = "pearson")
   hist(presid)
   fitted.glmm <- fitted(model, level=1)        # Extract the fitted (predicted) values
   plot(presid ~ fitted.glmm)                   # Check for homoscedasticity
-  
+  plot(presid ~ variable1)
+  plot(presid ~ variable2)
 }
 
 
-chkres.zi <- function(model) {
+chkres.zi <- function(model,variable1,variable2) {
   require(RVAideMemoire)
   presid <- residuals(model)
   hist(presid)
@@ -31,4 +34,6 @@ chkres.zi <- function(model) {
   plot(presid ~ fitted.glmm)                   # Check for homoscedasticity
   require(arm)
   binnedplot(fitted.glmm,presid)
+  plot(presid ~ variable1)
+  plot(presid ~ variable2)
 }
